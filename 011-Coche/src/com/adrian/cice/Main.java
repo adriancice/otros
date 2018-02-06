3e6b3528cd9d5b04dce6c898555d9b31743e7f62
Adrian <naikonpaul@gmail.com>
2018-02-06 15:18:20 +0100

    coches

--- 011-Coche/src/com/adrian/cice/Main.java
+++ 011-Coche/src/com/adrian/cice/Main.java
@@ -10,6 +10,9 @@ public class Main {
 		System.out.println("El modelo del coche es: " + coche1.getModelo());
 		System.out.println("El color del coche es: " + coche1.getColor());
 		System.out.println(coche1.getPinturaMetalizada() + " tiene la pintura metalizada");
+		
+		Coche coche2 = new Coche("Opel", "Astra", "1444BRH", "familiar", "terceros", "No", 2001);
+		System.out.println(coche2.getAñoFabricacion());
 	}
 
 }
